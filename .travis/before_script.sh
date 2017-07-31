23420284d95773d603570b244c8981aed1463f3a
kj <kjclev@gmail.com>
2017-08-01 07:41:45 +0900

    Updated test script

--- .travis/before_script.sh
+++ .travis/before_script.sh
@@ -12,6 +12,12 @@ sudo cp -f ${conf} ${default}
 sudo mkdir -p /var/www/html/
 sudo cp -R ${TRAVIS_BUILD_DIR}/tests/html/* /var/www/html/
 
+printf '.%.0s' {1..65537} | sudo tee -a /var/www/html/br1/test01.txt
+printf '.%.0s' {1..65537} | sudo tee -a /var/www/html/br1/test02.html
+printf '.%.0s' {1..65537} | sudo tee -a /var/www/html/br2/test03.txt
+printf '.%.0s' {1..65537} | sudo tee -a /var/www/html/br2/test04.html
+printf '.%.0s' {1..65537} | sudo tee -a /var/www/html/br2/test05.htm
+
 sudo service apache2 restart
 
 curl -sI localhost
